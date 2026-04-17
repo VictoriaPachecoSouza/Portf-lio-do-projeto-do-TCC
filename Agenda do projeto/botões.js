d873368cfc245ae7bf0f9d583683be79df3e3c0b
Victória <Victoria.pacheco2606@gmail.com>
2026-04-17 11:23:07 -0300

    atualização de código

--- Agenda do projeto/botões.js
+++ Agenda do projeto/botões.js
@@ -1,19 +1,5 @@
-function toggleMenu(){
-    document.getElementById("menu").classList.toggle("active");
-}
-
-function openModal(){
-    document.getElementById("loginModal").style.display="flex";
-}
-
-window.onclick = function(event){
-    let modal = document.getElementById("loginModal");
-    if(event.target == modal){
-        modal.style.display="none";
-    }
-}
-
-function goToCart(){
-    alert("Redirecionando para o carrinho...");
-    window.location.href="#";
-}
+document.getElementById('btnAbrir').addEventListener('click', function() {
+  var input = document.getElementById('meuCalendario');
+  input.showPicker(); // Abre o calendário nativo
+});
+ 
