7c86004db7559c59e4eaabea3ed573ad011a924b
Victória <Victoria.pacheco2606@gmail.com>
2026-06-02 14:13:36 -0300

    Add files via upload
    Retirada do botão de login.
    o erro do calendário continua.

---
+++ Agenda_do_projeto/botões.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("active");
+}
+
+function openModal() {
+    document.getElementById("loginModal").style.display = "flex";
+}
+
+window.onclick = function(event) {
+    let modal = document.getElementById("loginModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function goToCart() {
+    alert("Redirecionando para o carrinho...");
+    window.location.href = "#";
+}
